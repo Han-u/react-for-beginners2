c14150dd3138cf2a2474a4fd3981c249f3f27eac
han <chos9715@naver.com>
2022-09-06 21:59:40 +0900

    coinTracker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import App2 from './App2';
+import CoinTracker from './CoinTracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,6 +10,8 @@ root.render(
     <App />
     <br></br>
     <App2 />
+    <br/>
+    <CoinTracker />
   </React.StrictMode>
 );
 
