496aea3fd829735b3eda119838dfca1be7d79df5
han <chos9715@naver.com>
2022-09-09 19:55:59 +0900

    Movie 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import App2 from './App2';
 import CoinTracker from './CoinTracker';
+import Movie from './Movie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,8 @@ root.render(
     <App2 />
     <br/>
     <CoinTracker />
+    <br></br>
+    <Movie />
   </React.StrictMode>
 );
 
